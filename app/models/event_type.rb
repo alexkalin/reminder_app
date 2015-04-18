f09b3731607af3796e207c8b271001314d64d989
Alex Kalinchuk <alex.kalinchuk@gmail.com>
2015-04-18 12:17:11 -0400

    Add devise and account events controller.

--- app/models/event_type.rb
+++ app/models/event_type.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: event_types
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class EventType < ActiveRecord::Base
+  has_many :event_type_plans
+  has_many :plans, through: :event_type_plans
 end
