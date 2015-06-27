291b1e5a8fe58ced7cea5ec17b2892d9b1c8aea1
Alex Kalinchuk <alex.kalinchuk@gmail.com>
2015-06-27 12:05:00 -0400

    Added settings/plans/credit cards.

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,3 @@
+require "stripe"
+Stripe.api_key = "sk_test_jLj7rD88VuhymM9uiXs567LU"
+Stripe.api_version = "2015-06-15"
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,16 @@ Rails.application.routes.draw do
   
   namespace :account do
     resources :events
+    resources :plans, only: [:index] do
+      member do
+        patch :switch
+      end
+    end
+    resources :credit_cards, only: [:new, :create]
+
+    scope :settings, controller: :settings do
+      get :edit
+      patch :update
+    end
   end
 end
